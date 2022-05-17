b9575e900084e757e745081ecde34078442e9bf6
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-05-17 00:12:14 -0700

    Add files via upload

--- Longest Palindrome.py
+++ Longest Palindrome.py
@@ -9,3 +9,12 @@ class Solution:
         odds = sum(v & 1 for v in collections.Counter(s).values())
         return len(s) - odds + bool(odds)
 
+        # Solution 2 hash set
+        hash = set()
+        for c in s:
+            if c not in hash:
+                hash.add(c)
+            else:
+                hash.remove(c)
+        # len(hash) is the number of the odd letters
+        return len(s) - len(hash) + 1 if len(hash) > 0 else len(s)
