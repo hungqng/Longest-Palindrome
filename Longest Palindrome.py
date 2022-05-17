36e739330c429e59b3504671b9062af6ee7049cc
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-05-17 00:12:01 -0700

    Add files via upload

---
+++ Longest Palindrome.py
@@ -0,0 +1,11 @@
+# 409. Longest Palindrome
+
+# Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+
+# Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        odds = sum(v & 1 for v in collections.Counter(s).values())
+        return len(s) - odds + bool(odds)
+
